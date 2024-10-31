5e1e082f6219b59d8765df364c41c9d239fea827
sachithrakalani <kalanisathyani@gmail.com>
2024-10-31 11:46:57 +0530

    Add home button and add property button

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,6 +2,9 @@ export default function HomePage(){
     return(
         <div>
             <h1>Find Your Dream Home</h1>
+            <h1>Real Estate</h1>
+            <button>Home</button>
+            <button>Add Property</button>
         </div>
     )
 }
