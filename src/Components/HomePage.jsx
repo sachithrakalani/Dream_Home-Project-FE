b35e66c2c0da48ce1bddf2ad26d07eb40540c003
sachithrakalani <kalanisathyani@gmail.com>
2024-10-31 16:19:39 +0530

    Add drop down list

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -8,6 +8,26 @@ export default function HomePage(){
             <button>Home</button>
             <button>Add Property</button>
             <img src={DreamHomeImage} alt="Dream Home" />
+            <h1>Looking to Buy or Rent a Property?</h1>
+            <h1>Find Your Dream Home</h1>
+            <select>
+                <option value="">All Main Locations: </option>
+                <option value="Colombo">Colombo</option>
+                <option value="Kandy">Kandy</option>
+                <option value="Galle">Galle</option>
+            </select>
+            <select>
+                <option value="">All Status: </option>
+                <option value="Colombo">Colombo</option>
+                <option value="Kandy">Kandy</option>
+                <option value="Galle">Galle</option>
+            </select>
+            <select>
+                <option value="">All Types: </option>
+                <option value="Colombo">Colombo</option>
+                <option value="Kandy">Kandy</option>
+                <option value="Galle">Galle</option>
+            </select>
         </div>
     )
 }
