0ba504ebfb9a3a17cf9ae3269423f25e5d0f1c9e
sachithrakalani <kalanisathyani@gmail.com>
2024-10-31 15:56:09 +0530

    Add image in home page

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,3 +1,5 @@
+import DreamHomeImage from "/Users/sachithrakalani/Workspace/React/Dream-Home-Project/dreamHomeReactProject/src/accets/images/DreamHomeImage.jpeg";
+
 export default function HomePage(){
     return(
         <div>
@@ -5,6 +7,7 @@ export default function HomePage(){
             <h1>Real Estate</h1>
             <button>Home</button>
             <button>Add Property</button>
+            <img src={DreamHomeImage} alt="Dream Home" />
         </div>
     )
 }
