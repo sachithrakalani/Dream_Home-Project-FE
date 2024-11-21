52cc1da1091ab11da3c63fb1eca62685ce5f6b1d
sachithrakalani <kalanisathyani@gmail.com>
2024-11-21 10:38:14 +0530

    Steyle Real Estate and home,add property buttons

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,44 +1,46 @@
 import DreamHomeImage from "/Users/sachithrakalani/Workspace/React/Dream-Home-Project/dreamHomeReactProject/src/accets/images/DreamHomeImage.jpeg";
-import styles from "./homepage.module.css"
+import styles from "./homepage.module.css";
+import bodystyles from "./homepagebody.module.css";
 
+export default function HomePage() {
+  return (
+    <div>
+      <div className={styles.title}>
+        <h1>Find Your Dream Home</h1>
+      </div>
+      <div className={bodystyles.header}>
+        <h1>Real Estate</h1>
+        <button>Home</button>
+        <button>Add Property</button>
+      </div>
 
-export default function HomePage(){
-    return(
-        <div>
-            <div className={styles.title}>
-            <h1>Find Your Dream Home</h1>
-            </div>
-            
-            <h1>Real Estate</h1>
-            <button>Home</button>
-            <button>Add Property</button>
-            <img src={DreamHomeImage} alt="Dream Home" />
-            <h1>Looking to Buy or Rent a Property?</h1>
-            <h1>Find Your Dream Home</h1>
-            <select>
-                <option value="">All Main Locations: </option>
-                <option value="Colombo">Colombo</option>
-                <option value="Kandy">Kandy</option>
-                <option value="Galle">Galle</option>
-            </select>
-            <select>
-                <option value="">All Status: </option>
-                <option value="Colombo">Colombo</option>
-                <option value="Kandy">Kandy</option>
-                <option value="Galle">Galle</option>
-            </select>
-            <select>
-                <option value="">All Types: </option>
-                <option value="Colombo">Colombo</option>
-                <option value="Kandy">Kandy</option>
-                <option value="Galle">Galle</option>
-            </select>
-            <button >
-                <i className="fas fa-search-plus"></i> 
-            </button>
-            <button>
-                <i className="fas fa-search"></i>  Search
-            </button>
-        </div>
-    )
+      <img src={DreamHomeImage} alt="Dream Home" />
+      <h1>Looking to Buy or Rent a Property?</h1>
+      <h1>Find Your Dream Home</h1>
+      <select>
+        <option value="">All Main Locations: </option>
+        <option value="Colombo">Colombo</option>
+        <option value="Kandy">Kandy</option>
+        <option value="Galle">Galle</option>
+      </select>
+      <select>
+        <option value="">All Status: </option>
+        <option value="Colombo">Colombo</option>
+        <option value="Kandy">Kandy</option>
+        <option value="Galle">Galle</option>
+      </select>
+      <select>
+        <option value="">All Types: </option>
+        <option value="Colombo">Colombo</option>
+        <option value="Kandy">Kandy</option>
+        <option value="Galle">Galle</option>
+      </select>
+      <button>
+        <i className="fas fa-search-plus"></i>
+      </button>
+      <button>
+        <i className="fas fa-search"></i> Search
+      </button>
+    </div>
+  );
 }
